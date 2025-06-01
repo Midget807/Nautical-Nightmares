f5426c18834bdd38be7fe6d81f45be2882b7c4ed
Midget807 <phu7mai@gmail.com>
2025-06-01 13:31:38 +0930

    blink

--- src/main/java/net/midget807/nautical_nightmares/mixin/client/InGameHudMixin.java
+++ src/main/java/net/midget807/nautical_nightmares/mixin/client/InGameHudMixin.java
@@ -7,10 +7,12 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.midget807.nautical_nightmares.entity.CanBePressurised;
 import net.midget807.nautical_nightmares.util.ModTextureIds;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
@@ -27,11 +29,14 @@ public abstract class InGameHudMixin {
     @Shadow @Final private MinecraftClient client;
 
     @Shadow @Nullable protected abstract PlayerEntity getCameraPlayer();
-/*
-    @Inject(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;push(Ljava/lang/String;)V"))
-    private void nauticalNightmare$render(DrawContext context, CallbackInfo ci, @Local PlayerEntity playerRef) {
-        renderPressure(context, playerRef);
-    }*/
+
+    @Shadow public abstract TextRenderer getTextRenderer();
+
+    /*
+        @Inject(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;push(Ljava/lang/String;)V"))
+        private void nauticalNightmare$render(DrawContext context, CallbackInfo ci, @Local PlayerEntity playerRef) {
+            renderPressure(context, playerRef);
+        }*/
     @Inject(method = "renderMainHud", at = @At("TAIL"))
     private void nauticalNightmare$renderMain(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
         renderPressure(context, this.getCameraPlayer());
@@ -55,6 +60,12 @@ public abstract class InGameHudMixin {
         }
         context.drawGuiTexture(ModTextureIds.PRESSURE_BAR_BG, i, j, 0, 81, 5);
         context.drawGuiTexture(ModTextureIds.PRESSURE_BAR_OVERLAY, i, j, 2, 81, 5);
+
+        if (((CanBePressurised)player).getPressurisedTicks() > 0) {
+            float lerpedAmount = MathHelper.abs((float) Math.tanh((3 * MathHelper.sin((float) ((CanBePressurised) player).getPressurisedTicks()))));//todo place correctly
+            int lerpedColor = ColorHelper.Argb.lerp(lerpedAmount, ColorHelper.Argb.getArgb(0, 255, 0, 0), ColorHelper.Argb.getArgb(255, 255, 0, 0));
+            context.drawText(this.getTextRenderer(), "Warning: Pressure Damage Imminent", context.getScaledWindowWidth() / 2, 30, lerpedColor, false);
+        }
         RenderSystem.disableBlend();
         this.client.getProfiler().pop();
     }
