af38cb88a23ddc8137139f4657c1721cff77ef0c
Midget807 <phu7mai@gmail.com>
2025-07-06 20:32:56 +0930

    jesse... we need to cook

--- src/main/java/net/midget807/nautical_nightmares/datagen/ModModelProvider.java
+++ src/main/java/net/midget807/nautical_nightmares/datagen/ModModelProvider.java
@@ -2,10 +2,12 @@ package net.midget807.nautical_nightmares.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.midget807.nautical_nightmares.registry.ModBlocks;
 import net.midget807.nautical_nightmares.registry.ModItems;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Models;
+import net.minecraft.block.Block;
+import net.minecraft.data.client.*;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.Identifier;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
